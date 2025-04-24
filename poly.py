d500fab2b126a3bb4e7eccceeb1ef99b6088633c
Kgothalang Isaac Lekitlane <isaacspeedy101@gmail.com>
2025-04-24 14:28:19 +0200

    Create poly.py

---
+++ poly.py
@@ -0,0 +1,24 @@
+
+class Vehicle:
+    def move(self):
+        print("This vehicle moves.")
+
+
+class Car(Vehicle):
+    def move(self):
+        print("Driving on the road! 🚗")
+
+class Plane(Vehicle):
+    def move(self):
+        print("Flying in the sky! ✈️")
+
+class Boat(Vehicle):
+    def move(self):
+        print("Sailing on the water! 🚤")
+
+
+vehicles = [Car(), Plane(), Boat()]
+
+
+for v in vehicles:
+    v.move()
